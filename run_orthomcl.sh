6002866a7f8987a272bc6383a2751b1ee5708811
Theodore DeRego <tderego94@gmail.com>
2015-08-18 10:52:05 -1000

    Specify run folder as argument

--- run_orthomcl.sh
+++ run_orthomcl.sh
@@ -1,3 +1,5 @@
+run_folder=$1
+
 # Start MySQL
 echo Starting MySQL container...
 docker run --name orthomcl-mysql -e MYSQL_ROOT_PASSWORD=asdf1234 -e MYSQL_USER=orthomcl_user -e MYSQL_PASSWORD=shhh_this_is_secret -e MYSQL_DATABASE=orthomcl -d mysql | tee .ortho_mcl_db_container_id | head -n -1
@@ -9,9 +11,9 @@ sleep 10
 
 # Start OrthoMCL container
 echo Starting OrthoMCL container...
-docker run -it --name orthomcl-run --link orthomcl-mysql:mysql -v `pwd`/Feb20:/ortho_mcl_run orthomcl /bin/bash
+docker run -it --name orthomcl-run --link orthomcl-mysql:mysql -v `pwd`/$run_folder:/ortho_mcl_run orthomcl /bin/bash
 
 # Clean up after MySQL container
-#docker kill `cat .ortho_mcl_db_container_id`
-#docker rm `cat .ortho_mcl_db_container_id`
-#rm .ortho_mcl_db_container_id
+docker kill `cat .ortho_mcl_db_container_id`
+docker rm `cat .ortho_mcl_db_container_id`
+rm .ortho_mcl_db_container_id
